ddbfbe2bc67ee9d195e2c2337dbbb0c8dd24896b
SeinalArifin149 <arifinseinals3@gmail.com>
2026-03-14 00:43:29 +0700

    add new db

--- backend/migration.py
+++ backend/migration.py
@@ -4,61 +4,71 @@ import sqlite3
 conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
 
+
+def get_or_create(table, column, value):
+
+    cursor.execute(
+        f"SELECT id_{table} FROM {table} WHERE {column}=?",
+        (value,)
+    )
+
+    result = cursor.fetchone()
+
+    if result:
+        return result[0]
+
+    cursor.execute(
+        f"INSERT INTO {table} ({column}) VALUES (?)",
+        (value,)
+    )
+
+    return cursor.lastrowid
+
+
 with open('data_jamu_274.csv', newline='', encoding='utf-8') as file:
+
     reader = csv.DictReader(file)
 
     for row in reader:
 
-        # cek kategori
-        cursor.execute(
-            "SELECT id_kategori FROM kategori WHERE nama_kategori=?",
-            (row["KATEGORI"],)
-        )
-
-        kategori = cursor.fetchone()
-
-        if kategori is None:
-            cursor.execute(
-                "INSERT INTO kategori (nama_kategori) VALUES (?)",
-                (row["KATEGORI"],)
-            )
-            id_kategori = cursor.lastrowid
-        else:
-            id_kategori = kategori[0]
-
-        # cek jenis
-        cursor.execute(
-            "SELECT id_jenis FROM jenis WHERE nama_jenis=?",
-            (row["JENIS"],)
-        )
-
-        jenis = cursor.fetchone()
-
-        if jenis is None:
-            cursor.execute(
-                "INSERT INTO jenis (nama_jenis) VALUES (?)",
-                (row["JENIS"],)
-            )
-            id_jenis = cursor.lastrowid
-        else:
-            id_jenis = jenis[0]
+        # ambil id dari tabel lain
+        id_kategori = get_or_create("kategori","nama_kategori",row["KATEGORI"])
+        id_jenis = get_or_create("jenis","nama_jenis",row["JENIS"])
+        id_produsen = get_or_create("produsen","nama_produsen",row["PRODUSEN"])
+        id_lokasi_produksi = get_or_create("lokasi_produksi","nama_lokasi",row["LOKASI PRODUKSI"])
+        id_kabupaten = get_or_create("kabupaten","nama_kabupaten",row["KABUPATEN"])
+        id_perizinan = get_or_create("perizinan","nama_perizinan",row["PERIZINAN"])
 
         # insert jamu
         cursor.execute("""
         INSERT INTO jamu (
-            nama_jamu, khasiat, kandungan, aturan_minum, efek_samping,
-            id_kategori, id_jenis
+            nama_jamu,
+            khasiat,
+            kandungan,
+            aturan_minum,
+            efek_samping,
+            id_kategori,
+            id_jenis,
+            id_produsen,
+            id_lokasi_produksi,
+            id_kabupaten,
+            id_perizinan
         )
-        VALUES (?, ?, ?, ?, ?, ?, ?)
-        """, (
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """,(
             row["NAMA JAMU"],
             row["KHASIAT"],
             row["KANDUNGAN"],
             row["ATURAN MINUM"],
             row["EFEK SAMPING"],
             id_kategori,
-            id_jenis
+            id_jenis,
+            id_produsen,
+            id_lokasi_produksi,
+            id_kabupaten,
+            id_perizinan
         ))
+
 
 conn.commit()
 conn.close()
